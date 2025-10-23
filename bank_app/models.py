410d5eecadf2fb756ed3d9ad52101f2bedc590ec
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-10-23 13:26:11 +0530

    required models are created

--- bank_app/models.py
+++ bank_app/models.py
@@ -3,4 +3,18 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+class User(AbstractUser):
+    account_number=models.CharField(max_length=18)
+    balance=models.DecimalField(default=0.0, max_digits=5, decimal_places=2)
+
+
+class Transactions(models.Model):
+    TRANSACTION_TYPE=(
+        ('credit', 'Credit'),
+        ('debit', 'Debit'),
+        )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions')
+    type = models.CharField(max_length=10, choices=TRANSACTION_TYPE)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
