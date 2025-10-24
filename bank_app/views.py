6f264a19a7176c459dc3851a45c38375289d1acb
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-10-24 14:30:06 +0530

    Views are created

--- bank_app/views.py
+++ bank_app/views.py
@@ -16,15 +16,61 @@ def get_token_for_user(user):
            'access':str(refresh.access_token)}
 
 # Registerview
-class Registration(APIView):
+class Registrationview(APIView):
+    def post(self,request):
+        username=request.data.get('username')
+        email=request.data.get('email')
+        account_number=request.data.get('account_number')
+        password=request.data.get('password')
+        balance=request.data.get('balance')
+        if User.objects.filter(username=username , account_number=account_number).exists():
+            return Response({"error":"User already exist"},status=status.HTTP_400_BAD_REQUEST)
+        user = User.objects.create_user(username=username,email=email,account_number=account_number,balance=balance ,password=password)
+        token =get_token_for_user(user)
+        return Response({"user": UserSerializer(user).data, "token": token})
+
+# Loginview
+class Loginview(APIView):
     def post(self,request):
         username=request.data.get('username')
         password=request.data.get('password')
-        if User.objects.filter(username=username).exists():
-            return Response({"error":"User already exist"},status=status.HTTP_400_BAD_REQUEST)
-        user = User.objects.create_user(username=username, password=password)
-        token =get_token_for_user(user)
+
+        user=authenticate(username=username,password=password)
+        if not user:
+            return Response({"error":"Invalid credentials"},status=status.HTTP_401_UNAUTHORIZED)
+        token=get_token_for_user(user)
         return Response({"user": UserSerializer(user).data, "token": token})
+    
+# Dashboard 
+class Dashboardview(APIView):
+    permission_class=[IsAuthenticated]
+    def get(self,request):
+        user=request.user
+        return Response(UserSerializer(user).data)
+    
+class Transactionview(APIView):
+    permission_class=[IsAuthenticated]
+    def post(self,request):
+        user=request.user
+        tr_type=request.data.get('type')
+        amount=float(request.data.get('amount'))
+        if tr_type=='debit' and user.balance<amount:
+            return Response({"error":"Insufficient balance"},status=status.HTTP_400_BAD_REQUEST)
+        elif tr_type=='credit':
+            user.balance+=amount
+        elif tr_type=='debit' and user.balance>amount:
+            user.balance-=amount
+        else:
+            return Response({'error':'Invalid transaction'})
+        user.save()
+        trans_det = Transactions.objects.create(user=user, type=tr_type, amount=amount)
+        return Response(TransactionSerializer(trans_det).data)
+
+
+
+
+
+
 
 
 
